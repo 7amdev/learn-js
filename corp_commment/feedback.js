b0249efdd4c67e9fb428eb6942b8a7ddbf8a93b9
Alfredo Monteiro <amonteiro.alfredo@gmail.com>
2023-09-19 14:15:48 -0100

    corp_comment(feat): create new feedback entry

--- corp_commment/feedback.js
+++ corp_commment/feedback.js
@@ -1,18 +1,90 @@
-const FEEDBACK = (function () {
+const feedback_m = (function () {
+  // @todo
+  // [] generate random color index
+
+  const feedbacks = [
+    {
+      company: "Starbucks",
+      votes: 593,
+      message: "I really wish #Starbucks would use wrappers for hot drinks as a standart, i keep burning my hands an am tired of bothering the employee.",
+      created_at: new Date("2023-09-15T03:24:00")
+    },
+    {
+      company: "Netflix",
+      votes: 422,
+      message: "Since yday on mobile #netflix keeps bufferig the video, it keeps happening even when i redownload the app. I'm in an area with decent internet connection.",
+      created_at: new Date("2023-09-16T03:24:00")
+    },
+    {
+      company: "Amazon",
+      votes: 310,
+      message: "Since yday on mobile #netflix keeps bufferig the video, it keeps happening even when i redownload the app. I'm in an area with decent internet connection.",
+      created_at: new Date("2023-09-17T03:24:00")
+    }
+  ];
+
+  const colors = ['#505da6', '#7550a6', '#508ba6'];
+  let feedbacks_ui = [];
+  let color_idx = 0;
+
   const init = function () {
     console.log('Initialize Feedback Module...');
   };
 
   const feedback_vote_click_handler = function () {
+    // @todo
+
     console.log('Vote item clicked....');
   };
 
-  const update_view = function () {
-    console.log('Update View....');
+  const feedback_template = function (data) {
+    const date_diff = Date.now() - new Date(data.created_at);
+    const date_diff_in_days = Math.floor(date_diff / (1000 * 3600 * 24));
+    
+    // if date_diff < 1 label = new
+    if (date_diff_in_days < 1) 
+
+    if (color_idx >= colors.length) color_idx = 0;
+
+    const markup = 
+      `<section tabindex="0" class="feedback">
+        <div class="feedback__container">
+          <button class="feedback__vote">
+            <i class="fas fa-sort-up feedback__upvote"></i>
+            <span class="feedback__count">${data.votes}</span>
+          </button>
+          <div class="feedback__initial-bck" style="background-color: ${colors[color_idx++]};">
+            <p class="feedback__initial">${data.company.charAt(0).toUpperCase()}</p>
+          </div>
+          <div class="feedback__message-wrapper">
+            <p class="feedback__company">${data.company}</p>
+            <p class="feedback__message">${data.message}</p>
+          </div>
+          <p class="feedback__time">${date_diff_in_days < 1 ? "new": date_diff_in_days + "d"}</p>
+        </div>
+      </section>`;
+    
+    return markup;
+  };
+
+  const feedback_list = function () {
+    feedbacks.forEach(function (feedback) {
+      feedbacks_ui.push(feedback_template(feedback));
+    });
+
+    return feedbacks_ui.join("");;
+  };
+
+  const feedback_add = function (new_entry) {
+    feedbacks.push(new_entry);
+
+    return feedback_template(new_entry);
   };
 
   return {
     init,
-    feedback_vote_click_handler
+    feedback_vote_click_handler,
+    feedback_list,
+    feedback_add
   };
 }());
--- corp_commment/index.js
+++ corp_commment/index.js
@@ -1,36 +1,68 @@
 const APP = (function () {
-  const form_el = document.querySelector('.form');
-  const form_textarea_el = document.querySelector('.form__textarea');
-  const form_count_el = document.querySelector('.form__count');
-  const feedback_vote_el = document.querySelector('.feedback__vote');
-  const hashtag_btn_els = document.querySelectorAll('.hashtag');
-  let form_character_limnit = 150;
+  const { 
+    feedback_list, 
+    feedback_vote_click_handler,
+    feedback_add
+  } = feedback_m;
+  const form_el             = document.querySelector('.form');
+  const form_textarea_el    = document.querySelector('.form__textarea');
+  const form_count_el       = document.querySelector('.form__count');
+  const feedbacks_el        = document.querySelector('.feedbacks');
+  const hashtag_btn_els     = document.querySelectorAll('.hashtag');
+  
+  let form_character_limit = 150;
+  let feedback_vote_els;
   
   const init = function () {
     console.log('Corpcomment intitialized...');
+
     // Initialization
-    form_count_el.textContent = form_character_limnit;
+    form_count_el.textContent = form_character_limit;
+    feedbacks_el.insertAdjacentHTML('beforeend', feedback_list());
+    feedback_vote_els = document.querySelectorAll('.feedback__vote');
 
     // Add event listeners
     form_el.addEventListener('submit', form_submit_handler);
-    form_textarea_el.addEventListener('input', form_textarea_input_handler);
-    feedback_vote_el.addEventListener('click', FEEDBACK.feedback_vote_click_handler);
+    form_textarea_el.addEventListener('input', form_textarea_input_handler);    
     hashtag_btn_els.forEach(function (hashtag_btn_el) {
       hashtag_btn_el.addEventListener('click', hashtag_click_handler);
     });
-    
-    // Initialize Feedback Module
-    FEEDBACK.init();
+    feedback_vote_els.forEach(function (feedback_vote_btn_el) {
+      feedback_vote_btn_el.addEventListener('click', feedback_vote_click_handler);
+    });
   };
 
-  const form_submit_handler = function (e) {
-    e.preventDefault();
+  const form_submit_handler = function (event) {
+    event.preventDefault();
 
-    console.log('Form has been submitted...');
+    const form_data = new FormData(form_el);
+    const company_name_rx = /#[a-z]+/g;
+    let new_entry = Object.fromEntries(form_data);
+    let company_name = new_entry.message.match(company_name_rx);
+
+    if (!company_name || company_name.length > 1) {
+      form_el.classList.remove('form--success');
+      form_el.classList.add('form--error');
+      return;
+    }
+
+    new_entry = {
+      ...new_entry,
+      votes: 0,
+      company: company_name[0].slice(1),
+      created_at: Date.now()
+    };
+
+    feedbacks_el.insertAdjacentHTML('beforeend', feedback_add(new_entry));
+    console.log(new_entry);
+    // POST feedback 
+    form_el.classList.add('form--success');
   };
 
   const form_textarea_input_handler = function (e) {
-    form_count_el.textContent = form_character_limnit - parseInt(form_textarea_el.value.length, 10);
+    const chararacter_length = form_character_limit - parseInt(form_textarea_el.value.length, 10);
+
+    form_count_el.textContent = chararacter_length; 
   };
   
   const hashtag_click_handler = function (e) {
