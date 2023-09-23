092c9c52cf37afc54bdbeb5bfbfd8e00ad0a19ae
Alfredo Monteiro <amonteiro.alfredo@gmail.com>
2023-09-23 10:19:59 -0100

    corp_comment(feat): form characters counter

--- corp_commment/index.js
+++ corp_commment/index.js
@@ -1,5 +1,13 @@
-const init = function () {
-  console.log('Corpcomment intitialized...');
+// GLOBALS 
+const textarea_el = document.querySelector('.form__textarea');
+const form_count_el = document.querySelector('.form__count');
+const characters_limit = 150;
+
+
+// COUNTER COMPONENT
+const textarea_input_handler = function (event) {
+  const text_value = textarea_el.value;
+  form_count_el.textContent = characters_limit - text_value.length;
 };
 
-document.addEventListener('DOMContentLoaded', init);
+textarea_el.addEventListener('input', textarea_input_handler);
